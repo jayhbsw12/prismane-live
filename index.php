15c9b0b6e05ec1e1d2777adcddc6f84af7a305b9
info@hbsoftweb.com <info@hbsoftweb.com>
2026-04-16 18:03:36 +0530

    work section images

--- index.php
+++ index.php
@@ -639,42 +639,42 @@
             <h2 class="portfolio__text">work</h2>
             <div class="portfolio__list-1">
               <div class="portfolio__item">
-                <a href="#"><img class="mover" src="assets/imgs/portfolio/1/work 1.webp" alt="Portfolio Image"></a>
+                <a href="#"><img class="mover" src="assets/imgs/DTF T-Shirt.png" alt="Portfolio Image"></a>
                 <!-- <div class="portfolio__info">
                     <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
                     <p>02 May 2021</p>
                   </div> -->
               </div>
               <div class="portfolio__item">
-                <a href="#"><img src="assets/imgs/portfolio/1/work 2.webp" alt="Portfolio Image"></a>
+                <a href="#"><img src="assets/imgs/Dye Sublimation.png" alt="Portfolio Image"></a>
                 <!-- <div class="portfolio__info">
                     <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
                     <p>02 May 2021</p>
                   </div> -->
               </div>
               <div class="portfolio__item">
-                <a href="#"><img src="assets/imgs/portfolio/1/work 3.webp" alt="Portfolio Image"></a>
+                <a href="#"><img src="assets/imgs/Direct to fabric.png" alt="Portfolio Image"></a>
                 <!-- <div class="portfolio__info">
                     <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
                     <p>02 May 2021</p>
                   </div> -->
               </div>
               <div class="portfolio__item">
-                <a href="#"><img src="assets/imgs/portfolio/1/work 4.webp" alt="Portfolio Image"></a>
+                <a href="#"><img src="assets/imgs/inks.png" alt="Portfolio Image"></a>
                 <!-- <div class="portfolio__info">
                     <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
                     <p>02 May 2021</p>
                   </div> -->
               </div>
               <div class="portfolio__item">
-                <a href="#"><img src="assets/imgs/portfolio/1/work 5.webp" alt="Portfolio Image"></a>
+                <a href="#"><img src="assets/imgs/t-shirt inks.png" alt="Portfolio Image"></a>
                 <!-- <div class="portfolio__info">
                     <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
                     <p>02 May 2021</p>
                   </div> -->
               </div>
               <div class="portfolio__item">
-                <a href="#"><img src="assets/imgs/portfolio/1/work 6.webp" alt="Portfolio Image"></a>
+                <a href="#"><img src="assets/imgs/Printers.webp" alt="Portfolio Image"></a>
                 <!-- <div class="portfolio__info">
                     <h3 class="portfolio__title">Arteck Lyon Conseil</h3>
                     <p>02 May 2021</p>
